24154477b79de8a40f338092370ae958435e46fb
Jonathan Wehrle <jonathan@pengubytes.de>
2023-12-04 23:34:40 +0100

    day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,10 +1,10 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::io::BufRead;
 use std::{env, fmt, fs::File, io, path::Path, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() < 2 {
         exit_with_usage();
     }
 
@@ -14,7 +14,12 @@ fn main() {
         exit_with_usage();
     }
 
-    let result = check_cards(&input_path);
+    let result = match args.len() {
+        2 => check_cards(&input_path),
+        3 => part2(&input_path),
+        _ => Err(CardError::from("Invalid number of arguments."))
+    };
+
     match result {
         Ok(sum) => println!("{sum}"),
         Err(error) => {
@@ -22,6 +27,52 @@ fn main() {
             exit_with_usage();
         }
     }
+}
+
+fn part2(input_file: &Path) -> Result<u32, CardError> {
+    let mut sum: u32 = 0;
+    let mut factors: VecDeque<u32> = VecDeque::new();
+
+    if let Ok(lines) = read_lines(input_file) {
+        for line in lines {
+            let line = match line {
+                Ok(line) => line,
+                Err(_) => return Err(CardError::from("Could not read line."))
+            };
+
+            let content = line.split_once(':').unwrap().1;
+            let (winning, own) = content.split_once('|').unwrap();
+            let winning_numbers: HashSet<u32> = winning.split_whitespace()
+                .map(str::parse::<u32>)
+                .filter_map(Result::ok)
+                .collect();
+            let own_numbers: HashSet<u32> = own.split_whitespace()
+                .map(str::parse::<u32>)
+                .filter_map(Result::ok)
+                .collect();
+
+            let current_factor = factors.pop_front().or(Some(1)).unwrap();
+            sum += current_factor;
+
+            let count = winning_numbers.intersection(&own_numbers).count();
+            if count > 0 {
+                if count > factors.len() {
+                    for _ in 0..(count - factors.len()) {
+                        factors.push_back(1);
+                    }
+                }
+                for _ in 0..current_factor {
+                    for (i, factor) in factors.iter_mut().enumerate() {
+                        *factor += 1;
+                        if i == count - 1 {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Ok(sum)
 }
 
 fn check_cards(input_file: &Path) -> Result<u32, CardError> {
@@ -53,6 +104,7 @@ fn check_cards(input_file: &Path) -> Result<u32, CardError> {
 
 fn exit_with_usage() {
     println!("Usage: day4 INPUT_FILE");
+    println!("Part2: day4 INPUT_FILE part2");
     process::exit(1);
 }
 
@@ -87,6 +139,12 @@ impl fmt::Display for CardError {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part2() {
+        let sum = part2(Path::new("tests/input.txt")).unwrap();
+        assert_eq!(sum, 30);
+    }
 
     #[test]
     fn test_check_cards() {
