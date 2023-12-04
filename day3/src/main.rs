c6e9a09cc47ba4cb550f446a30c05ad7761ec782
Jonathan Wehrle <jonathan@pengubytes.de>
2023-12-04 21:32:33 +0100

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,7 +5,13 @@ use std::{env, fmt, fs::File, io, path::Path, process};
 static RADIX: u32 = 10;
 
 fn main() {
+    // Better don't look at this :D
+
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 || args.len() > 3 {
+        exit_with_usage();
+    }
+
     let input_path = Path::new(&args[1]);
     if !input_path.exists() {
         eprintln!("Input file does not exist: {}", input_path.display());
@@ -14,6 +20,7 @@ fn main() {
 
     let result = match args.len() {
         2 => sum_of_parts(input_path),
+        3 => gear_ratio(input_path),
         _ => Err(SchematicError::from("Invalid number of arguments"))
     };
     match result {
@@ -25,8 +32,8 @@ fn main() {
     }
 }
 
-fn sum_of_parts(input_file: &Path) -> Result<u64, SchematicError> {
-    let mut sum: u64 = 0;
+fn sum_of_parts(input_file: &Path) -> Result<u32, SchematicError> {
+    let mut sum: u32 = 0;
 
     if let Ok(mut lines) = read_lines(input_file) {
         let mut window: VecDeque<Vec<char>> = VecDeque::with_capacity(3);
@@ -63,9 +70,9 @@ enum ParseState {
     PartNumber
 }
 
-fn parse_line(window: &VecDeque<Vec<char>>) -> Result<u64, SchematicError> {
-    let mut current_sum: u64 = 0;
-    let mut current_number: u64 = 0;
+fn parse_line(window: &VecDeque<Vec<char>>) -> Result<u32, SchematicError> {
+    let mut current_sum: u32 = 0;
+    let mut current_number: u32 = 0;
     let mut start: usize = 0;
 
     let mut state = ParseState::Searching;
@@ -96,7 +103,7 @@ fn parse_line(window: &VecDeque<Vec<char>>) -> Result<u64, SchematicError> {
             
         } else if c.is_digit(RADIX) {
             let new_digit = c.to_digit(RADIX)
-                    .ok_or(SchematicError::from("Could not read digit."))? as u64;
+                    .ok_or(SchematicError::from("Could not read digit."))?;
             current_number = current_number * 10 + new_digit;
             
             if state == ParseState::Searching || state == ParseState::Symbol {
@@ -141,8 +148,107 @@ fn symbol_in_range(line: &Vec<char>, start: usize, end: usize) -> bool {
     false
 }
 
+fn gear_ratio(input_file: &Path) -> Result<u32, SchematicError> {
+    let mut sum: u32 = 0;
+
+    if let Ok(mut lines) = read_lines(input_file) {
+        let mut window: VecDeque<Vec<char>> = VecDeque::with_capacity(3);
+        let line = match lines.next() {
+            None => return Err(SchematicError::from("Could not read line.")),
+            Some(line) => match line {
+                Ok(line) => line,
+                Err(_) => return Err(SchematicError::from("Could not read line."))
+            }
+        };
+        window.push_back(line.chars().collect());
+        for line in lines {
+            let line = match line {
+                Ok(line) => line,
+                Err(_) => return Err(SchematicError::from("Could not read line."))
+            };
+            window.push_back(line.chars().collect());
+            sum += parse_for_gears(&window)?;
+        }
+        let tmp = window.pop_front()
+            .ok_or(SchematicError::from("Could not parse last line."))?;
+        window.push_back(tmp);
+        sum += parse_for_gears(&window)?;
+    }
+
+    Ok(sum)
+}
+
+fn parse_for_gears(window: &VecDeque<Vec<char>>) -> Result<u32, SchematicError> {
+    let mut current_sum: u32 = 0;
+
+    let center = window.len() - 2;
+
+    let current_line = &window[center];
+    for (i, c) in current_line.iter().enumerate() {
+        if c == &'*' {
+            let mut numbers: Vec<u32> = Vec::new();
+            numbers.append(&mut search_numbers(&window[center + 1], i));
+            if center > 0 {
+                numbers.append(&mut search_numbers(&window[center - 1], i));
+            }
+            numbers.append(&mut search_numbers(&current_line, i));
+
+            if numbers.len() == 2 {
+                current_sum += numbers[0] * numbers[1];
+            }
+        }
+    }
+    Ok(current_sum)
+}
+
+fn search_numbers(line: &Vec<char>, pos: usize) -> Vec<u32> {
+    let mut result: Vec<u32> = Vec::new();
+    let mut current: u32 = 0;
+    let mut state = ParseState::Searching;
+
+    if pos > 0 && line[pos - 1].is_digit(RADIX) {
+        state = ParseState::Number;
+        current = line[pos - 1].to_digit(RADIX).unwrap();
+        if pos > 1 {
+            let mut  factor: u32 = 10;
+            for i in (0..(pos - 1)).rev() {
+                if line[i].is_digit(RADIX) {
+                    current += line[i].to_digit(RADIX).unwrap() * factor;
+                    factor *= 10;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    
+    for (i, c) in line[pos..].iter().enumerate() {
+        if state == ParseState::Number {
+            if c.is_digit(RADIX) {
+                current = current * 10 + c.to_digit(RADIX).unwrap();
+            } else {
+                result.push(current);
+                current = 0;
+                state = ParseState::Searching;
+            }
+        } else {
+            if i > 1 {
+                break;
+            } else if c.is_digit(RADIX) {
+                current = c.to_digit(RADIX).unwrap();
+                state = ParseState::Number;
+            }
+        }
+    }
+    if state == ParseState::Number {
+        result.push(current)
+    }
+    result
+}
+
 fn exit_with_usage() {
     println!("Usage: day3 INPUT_FILE");
+    println!("Part2: day3 INPUT_FILE part2");
     process::exit(1);
 }
 
@@ -177,6 +283,25 @@ impl fmt::Display for SchematicError {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_gear_ratio() {
+        let sum = gear_ratio(Path::new("tests/input.txt")).unwrap();
+        assert_eq!(sum, 467835);
+    }
+
+    #[test]
+    fn test_search_numbers() {
+        let line: Vec<char> = "..31*11".chars().collect();
+        assert_eq!(search_numbers(&line, 4), vec![31, 11]);
+    }
+
+    #[test]
+    fn test_search_numbers2() {
+        let line = ".1234.5".chars().collect();
+        assert_eq!(search_numbers(&line, 3), vec![1234]);
+    }
+
     #[test]
     fn test_sum_of_parts() {
         let sum = sum_of_parts(Path::new("tests/input.txt")).unwrap();
