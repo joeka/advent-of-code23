73af448bb79d35053530f36a912b1ad64949ccc4
Jonathan Wehrle <jonathan@pengubytes.de>
2023-12-10 11:02:12 +0100

    day8 part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,6 +1,7 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io::BufRead;
 use std::{env, fmt, fs::File, io, path::Path, process};
+use num::integer::lcm;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,8 +15,9 @@ fn main() {
         exit_with_usage();
     }
 
-    let result: Result<u32, GhostError> = match args.len() {
+    let result: Result<u64, GhostError> = match args.len() {
         2 => get_out(&input_path),
+        3 => part2(&input_path),
         _ => Err(GhostError::from("Invalid number of arguments."))
     };
 
@@ -28,8 +30,68 @@ fn main() {
     }
 }
 
-fn get_out(input_file: &Path) -> Result<u32, GhostError> {
-    let mut steps: u32 = 0;
+fn part2(input_file: &Path) -> Result<u64, GhostError> {
+    let mut steps: u64 = 0;
+    let mut current: Vec<String> = Vec::new();
+    let mut graph: HashMap<String, (String, String)> = HashMap::new();
+    if let Ok(mut lines) = read_lines(input_file) {
+        let directions: Vec<char> = lines.next().unwrap().unwrap()
+            .chars()
+            .collect();
+        lines.next();
+
+        for line in lines {
+            if let Ok(line) = line {
+                let (mut node, rest) = line.split_once('=').unwrap();
+                node = node.trim();
+                let choices: (&str, &str) = rest[2..(rest.len()-1)].split_once(", ").unwrap();
+                graph.insert(node.to_owned(), (choices.0.to_owned(), choices.1.to_owned()));
+                
+                if node.chars().last().unwrap() == 'A' {
+                    current.push(node.to_owned());
+                }
+            }
+        }
+
+        let mut ghost_steps: HashSet<u64> = HashSet::new();
+
+        for direction in directions.iter().cycle() {
+            steps += 1;
+            let mut i: usize = 0;
+            while i < current.len() {
+                let node_id = current.get(i).unwrap().clone();
+                let node = graph.get(&node_id).unwrap();
+                let next_node = if *direction == 'L' {node.0.clone()} else {node.1.clone()};
+                
+                if next_node.chars().last().unwrap() == 'Z' {
+                    ghost_steps.insert(steps);
+                    current.remove(i);
+                } else {
+                    current[i] = next_node;
+                    i += 1;
+                }
+            }   
+
+            if current.is_empty() {
+                break;
+            }    
+        }
+
+        let mut iter = ghost_steps.iter();
+        steps = *iter.next().unwrap();
+
+        for ghost in ghost_steps {
+            steps = lcm(steps, ghost);
+        }
+        return Ok(steps)
+    }
+    
+   
+    Err(GhostError::from("Something went wrong"))
+}
+
+fn get_out(input_file: &Path) -> Result<u64, GhostError> {
+    let mut steps: u64 = 0;
     let mut graph: HashMap<String, (String, String)> = HashMap::new();
     if let Ok(mut lines) = read_lines(input_file) {
         let directions: Vec<char> = lines.next().unwrap().unwrap()
@@ -98,6 +160,12 @@ impl fmt::Display for GhostError {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part2() {
+        let steps = part2(Path::new("tests/part2.txt")).unwrap();
+        assert_eq!(steps, 6);
+    }
 
     #[test]
     fn test_example1() {
