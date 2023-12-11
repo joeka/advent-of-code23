5b6f4a402115240c80466cd775eb11d46e3b2583
Jonathan Wehrle <jonathan@pengubytes.de>
2023-12-11 19:56:02 +0100

    day11 part1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,11 +1,13 @@
-use std::path::Path;
-use aoc::{get_args, Part, exit_with_error};
+use std::path::PathBuf;
+use std::usize;
+
+use aoc::{get_args, Part, exit_with_error, get_input_buffer};
 use aoc::errors::AOCError;
 
 fn main() {
     let options = get_args();
 
-    let result: Result<u64, AOCError> = match options.part {
+    let result: Result<usize, AOCError> = match options.part {
         Part::One => part1(&options.input),
         Part::Two => part2(&options.input)
     };
@@ -16,15 +18,78 @@ fn main() {
     }
 }
 
+fn part1(input_file: &PathBuf) -> Result<usize, AOCError> {
+    let mut distance: usize = 0;
 
-fn part1(_: &Path) -> Result<u64, AOCError> {
+    let mut columns: Vec<Vec<Coordinate>> = Vec::new();
+
+    let mut y: usize = 0;
+    for line in get_input_buffer(input_file) {
+        if let Ok(line) = line {
+            let mut x: usize = 0;
+            let mut empty_line = true;
+            for c in line.chars() {
+                if columns.len() <= x {
+                    columns.push(Vec::new());
+                }
+                if c == '#' {
+                    columns[x].push(Coordinate{ x, y });
+                    empty_line = false;
+                }
+                x += 1;
+            }
+            y += if empty_line {2} else {1};
+        }
+    }
+    let mut offset = 0;
+    for column in columns.iter_mut() {
+        if column.is_empty() {
+            offset += 1;
+        } else if offset > 0 {
+            for galaxy in column.iter_mut() {
+                galaxy.x += offset;
+            }
+        }
+    }
+    for (i, a) in columns.iter().flatten().enumerate() {
+        for b in columns.iter().flatten().skip(1 + i) {
+            let dist = a.dist(b);
+            distance += dist;
+        }
+    }
+
+    Ok(distance)
+}
+
+fn part2(_: &PathBuf) -> Result<usize, AOCError> {
     Err(AOCError::from("Not implemented"))
 }
 
-fn part2(_: &Path) -> Result<u64, AOCError> {
-    Err(AOCError::from("Not implemented"))
+#[derive(Clone, PartialEq, Debug)]
+struct Coordinate {
+    x: usize,
+    y: usize
 }
 
+impl Coordinate {
+    pub fn dist(&self, other: &Coordinate) -> usize {
+        if self == other {
+            0
+        } else {
+            let x_diff = if self.x > other.x {
+                self.x - other.x
+            } else {
+                other.x - self.x
+            };
+            let y_diff = if self.y > other.y {
+                self.y - other.y
+            } else {
+                other.y - self.y
+            };
+            x_diff + y_diff
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -32,8 +97,8 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let value = part1(Path::new("tests/input.txt")).unwrap();
-        assert_eq!(value, 42);
+        let value = part1(&PathBuf::from("tests/input.txt")).unwrap();
+        assert_eq!(value, 374);
     }
 
 }
